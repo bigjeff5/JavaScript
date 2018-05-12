f72cad2b20359d153afdc6c84df4e9bd9f4c2212
Wells <Jeff.Wells@bp.com>
2018-05-11 16:33:45 -0800

    Updated craft.js

--- Craft Game/Craft.js
+++ Craft Game/Craft.js
@@ -5,11 +5,22 @@ var CANVAS_HEIGHT = 480;
 var canvas = document.getElementById('craft');
 var ctx = canvas.getContext('2d');
 
-draw()
+// Game Loop
+window.requestAnimationFrame(function() {
+    draw();
+});
+
+// bouncy-ball object
+
 
 // Core Draw function
 function draw() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.fillStyle = 'rgb(200, 0, 0)';
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+// Core Update function
+function update() {
+
 }
